44e62e157af223cf8f3e987c066abc96ee5bf6d2
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-09-21 09:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-document.getElementById("enterBtn").addEventListener("click", function() {
-	// Get the reference to the <p> tag 
-	var statusParagraph = document.getElementById("status");
+// document.getElementById("enterBtn").addEventListener("click", function() {
+// 	// Get the reference to the <p> tag 
+// 	var statusParagraph = document.getElementById("status");
 	
-	// Change its text content
-	statusParagraph.textContent = "Entered Metaverse";
+// 	// Change its text content
+// 	statusParagraph.textContent = "Entered Metaverse";
 
-	// Display the text in an <h1> tag 
-	document.getElementById("status").textContent = statusParagraph.textContent;
+// 	// Display the text in an <h1> tag 
+// 	document.getElementById("status").textContent = statusParagraph.textContent;
+// });
+// //your JS code here. If required.
+
+
+document.getElementById("enterBtn").addEventListener("click", function () {
+	var p = document.getElementById("status");
+	p.innerHTML = "<h1>Entered Metaverse</h1>";
+
 });
-//your JS code here. If required.
